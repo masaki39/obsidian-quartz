1b676984890b165bb6cd12d7e057d9bebf6054f7
masaki39 <88765754+masaki39@users.noreply.github.com>
2025-07-26 00:49:01 +0900

    Update quartz.config.ts
    aliasredirectsをオフにした

--- quartz.config.ts
+++ quartz.config.ts
@@ -75,7 +75,7 @@ const config: QuartzConfig = {
     ],
     filters: [Plugin.RemoveDrafts()],
     emitters: [
-      Plugin.AliasRedirects(),
+      // Plugin.AliasRedirects(),
       Plugin.ComponentResources(),
       Plugin.ContentPage(),
       Plugin.FolderPage(),
